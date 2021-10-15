1052df2fe255d91892852985450af915977f4698
Diego <pestanadiegoalberto@gmail.com>
2021-10-15 19:58:32 -0400

    v2.0

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 let buttons = document.querySelectorAll('button');
 const input = document.querySelector('input');
+let completed = false;
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         buttonContent = button.innerText;
-        if(buttonContent == '=') {
-            doOperation(input);
-        } else if(!(buttonContent == 'C')) {
-            input.value += buttonContent;
+        if(!completed) {
+            if(buttonContent == '=') {
+                doOperation(input);
+            } else if(!(buttonContent == 'C')) {
+                input.value += buttonContent;
+            } else {
+                input.value = ' ';
+            };
         } else {
             input.value = ' ';
+            completed = false;
         };
     }); 
 });
@@ -18,5 +24,6 @@ function doOperation(input) {
     operation = input.value;
     result = eval(operation);
     input.value = result;
+    completed = true;
 }
 
